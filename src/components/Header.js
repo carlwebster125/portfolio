92d4f3a3b36293837c7f047113d590a97af0d4fd
Carl Webster <carlwebster125@gmail.com>
2024-01-31 02:28:28 -0500

    Ready for Deployment

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Carl Webster</h1>
+      <p>Full-Stack Web Developer</p>
+    </header>
+  );
+};
+
+export default Header;

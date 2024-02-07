da2eb661992781936f4ce2035dc1676ca8393811
Carl Webster <carlwebster125@gmail.com>
2024-02-07 13:30:33 -0500

    change to vite

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Projects from './components/Projects'
+
+function App() {
+
+  return (
+    <>
+    <main>
+  <Projects/>
+  </main>
+    </>
+  )
+}
+
+export default App
